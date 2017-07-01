d1208934001864513913e862f44a19a75e3f82ab
Gathua Kiragu <kiragugathua@gmail.com>
2017-07-01 14:49:55 +0300

    Designed the user login layout

---
+++ app/src/main/java/com/example/kiragu/maua_chapchap/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.kiragu.maua_chapchap.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kiragu.maua_chapchap.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
