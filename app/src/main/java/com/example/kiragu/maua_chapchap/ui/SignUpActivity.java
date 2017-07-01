243a2bd76e067e7cd5d31475053af3203cd8ac63
Gathua Kiragu <kiragugathua@gmail.com>
2017-07-01 14:54:23 +0300

    Added an empty activity to sign up user

---
+++ app/src/main/java/com/example/kiragu/maua_chapchap/ui/SignUpActivity.java
@@ -0,0 +1,17 @@
+package com.example.kiragu.maua_chapchap.ui;
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kiragu.maua_chapchap.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
