345931e0e46f2dcda2b0662314294b2e854f1691
GathuaKiragu <kiragugathua@gmail.com>
2017-09-26 18:12:38 +0300

    Added a signup and login activity

---
+++ app/src/main/java/com/example/kiragu/maua_chapchap/Ui/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.example.kiragu.maua_chapchap.Ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kiragu.maua_chapchap.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
