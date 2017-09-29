a03462ac94d59843dfa9fb23138bda5571cf4488
GathuaKiragu <kiragugathua@gmail.com>
2017-09-29 12:43:40 +0300

    Added an intro slider

--- app/src/main/java/com/example/kiragu/maua_chapchap/Ui/IntroActivity.java
+++ app/src/main/java/com/example/kiragu/maua_chapchap/Ui/IntroActivity.java
@@ -1,15 +1,72 @@
 package com.example.kiragu.maua_chapchap.Ui;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.kiragu.maua_chapchap.R;
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntro2;
+import com.github.paolorotolo.appintro.AppIntroFragment;
 
-public class IntroActivity extends AppCompatActivity {
+
+
+public class IntroActivity extends AppIntro {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Note here that we DO NOT use setContentView();
+
+        // Add your slide fragments here.
+        // AppIntro will automatically generate the dots indicator and buttons.
+//        addSlide(firstFragment);
+//        addSlide(secondFragment);
+//        addSlide(thirdFragment);
+//        addSlide(fourthFragment);
+
+        // Instead of fragments, you can also use our default slide
+        // Just set a title, description, background and image. AppIntro will do the rest.
+        addSlide(AppIntroFragment.newInstance("Phancy Flowers", "Welcome to Phancy Flowers", R.drawable.logo , 000000));
+        addSlide(AppIntroFragment.newInstance("Phancy Flowers", "Welcome to Phancy Flowers", R.drawable.logo , 48220255));
+        addSlide(AppIntroFragment.newInstance("Phancy Flowers", "Welcome to Phancy Flowers", R.drawable.logo , 48255186));
+        addSlide(AppIntroFragment.newInstance("Phancy Flowers", "Welcome to Phancy Flowers", R.drawable.logo , 17925548));
+
+        // OPTIONAL METHODS
+        // Override bar/separator color.
+        setBarColor(Color.parseColor("#3F51B5"));
+        setSeparatorColor(Color.parseColor("#2196F3"));
+
+        // Hide Skip/Done button.
+        showSkipButton(true);
+        setProgressButtonEnabled(true);
+
+        // Turn vibration on and set intensity.
+        // NOTE: you will probably need to ask VIBRATE permission in Manifest.
+        setVibrate(true);
+        setVibrateIntensity(30);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        Intent intent = new Intent(IntroActivity.this, ProductList.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+        startActivity(intent);    }
+
+    @Override
+    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+        super.onSlideChanged(oldFragment, newFragment);
+        // Do something when the slide changes.
     }
 }
+
